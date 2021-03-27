83dd8e9d032cbb65850903ed385dddeebfcd65c3
Mikhail <mmas6150@gmail.com>
2021-03-27 17:22:08 +0300

    DB impl

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from datetime import datetime
 from flask import Flask
+import uuid
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SexHack.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/<login>&<password>', methods=["POST"])
-def try_login(login, password):
-    return True
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String(36), unique=True, primary_key=True)
+    login = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    task_count = db.Column(db.TEXT, default="[]")
+
+
+def register_user(login, password):
+    user = User(id=str(uuid.uuid4()), login=login, password=password)
+    db.session.add()
+    db.session.commit()
+
+
+def login_user(login, password):
+    pass
+
+
+
+@app.route('/login/login=<login>&password=<password>', methods=["POST"])
+def login(login, password):
+    token = login(login, password)
+
+
+@app.route('/register/login=<login>&password=<password>', methods=["POST"])
+def register_user(login, password):
+    register_user(login, password)
 
 
 @app.route('/time', methods=["GET"])
