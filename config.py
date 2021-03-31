3e2f8bc19a7db4e13d6a586d25ba8f8fdd54d120
Mikhail <mmas6150@gmail.com>
2021-04-01 01:27:37 +0300

    refactor server

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sys import platform
 
 if platform == "linux" or platform == "linux2":
     db_path = 'sqlite:////home/mika/resources/data/SexHack.sqlite'
-    resource = r"/home/mika/resources/databases"
+    resource = r"/home/mika/resources/images"
 elif platform == "win32":
     db_path = 'sqlite:///SexHack.sqlite'
     resource = r"C:\Users\mmas6\PycharmProjects\SexHack\static\images"
